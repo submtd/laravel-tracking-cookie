6b3dc8cf2a47978ae1c12ab442b4b988649b6169
Steve Harmeyer <harmeyersteve@gmail.com>
2019-03-23 09:14:42 -0700

    updates

--- config/laravel-tracking-cookie.php
+++ config/laravel-tracking-cookie.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
-    'trackingCookieName' => 'tracking', // name of the cookie
-    'trackingCookieLifetime' => 43200, // 30 days
+    'honorDoNotTrack' => true,
+    'cookieName' => 'tracking', // name of the cookie
+    'cookieLifetime' => 43200, // 30 days
+    'cookiePath' => '/',
     'useSecureCookie' => false, // only set the cookie over https connections
     'httpOnly' => false, // only allow the cookie to be read over http (no javascript, etc)
 ];
--- routes/routes.php
+++ routes/routes.php
@@ -1,20 +1,36 @@
 <?php
 
-Route::any('track/{path}', function ($path) {
+Route::any('track/{path?}', function ($path = null) {
+    // honor do not track header
+    if (config('laravel-tracking-cookie.honorDoNotTrack', true) && request()->header('Dnt')) {
+        return response()->redirectTo($path ?? '/');
+    }
     // see if cookie is already set
-    if (!$data = \Illuminate\Support\Facades\Cookie::get('tracking')) {
+    if (!$data = request()->cookie(config('laravel-tracking-cookie.cookieName', 'tracking'))) {
         $data = json_encode(request()->all());
     }
-    // set the cookie
-    \Illuminate\Support\Facades\Cookie::queue(
-        'tracking', // name
+    // redirect to the path provided
+    return response()->redirectTo($path ?? '/')->cookie(
+        config('laravel-tracking-cookie.cookieName', 'tracking'), // name
         $data, // value
-        config('laravel-tracking-cookie.trackingCookieLifetime', 43200), // minutes
-        '/', // path
+        config('laravel-tracking-cookie.cookieLifetime', 43200), // expires
+        config('laravel-tracking-cookie.cookiePath', '/'), // path
         implode('.', array_slice(explode('.', request()->getHost()), -2, 2)),
         config('laravel-tracking-cookie.useSecureCookie', false),
         config('laravel-tracking-cookie.httpOnly', false)
     );
-    // redirect to the path provided
-    return response()->redirectTo($path);
 })->where('path', '.*');
+
+Route::get('readtracking', function () {
+    return request()->cookie(config('laravel-tracking-cookie.cookieName', 'tracking'));
+});
+
+Route::get('removetracking', function () {
+    return response('removed')->cookie(
+        config('laravel-tracking-cookie.cookieName', 'tracking'),
+        '',
+        -36000,
+        config('laravel-tracking-cookie.cookiePath', '/'),
+        implode('.', array_slice(explode('.', request()->getHost()), -2, 2))
+    );
+});
